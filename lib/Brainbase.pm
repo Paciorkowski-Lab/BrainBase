a377e8d954bfa8c7e76261272770037ebf3d2eb4
alex-paciorkowski <alex.paciorkowski@gmail.com>
2016-02-15 10:01:59 -0900

    Create Brainbase.pm

---
+++ lib/Brainbase.pm
@@ -0,0 +1,14 @@
+L<Brainbase::Controller::Root>, L<Catalyst>
+
+=head1 AUTHOR
+
+Alex Paciorkowski
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
